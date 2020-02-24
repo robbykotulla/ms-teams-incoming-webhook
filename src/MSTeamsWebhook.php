ea202fb41cd23a5ed19d0c513e6d0418af6a570a
Robby Kotulla <robby.kotulla@gmail.com>
2020-02-24 09:07:58 +0100

    Create MSTeamsWebhook.php

---
+++ src/MSTeamsWebhook.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace robbykotulla/MSTeamsWebhook;
+
+class MSTeamsWebhook
+{
+
+  /**
+   * Constructor.
+   *
+   */
+  public function __construct()
+  {
+  }
+
+  /**
+   * Destructor.
+   */
+  public function __destruct()
+  {
+  }
+}
